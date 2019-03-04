dc106eea31e6ed60632c60d587c156689148b047
Job <jobsmith9@gmail.com>
2019-03-04 11:51:18 -0700

    initial commit

---
+++ src/scenes/Scene1.vue
@@ -0,0 +1,44 @@
+<template>
+<Scene name="winter">
+    <square name="floor"/>
+    <SnowFlake pool=200/>
+    <Tree/>
+    <polygon name="snowman">
+        <sphere name="head"/> 
+        <sphere name="body"/>
+        <sphere name="legs"/>
+        <cylinder name="leftArm"/>
+        <cylinder name="rightArm"/>
+    </polygon>
+</Scene>
+</template>
+
+<script>
+// @ is an alias to /src
+import SnowFlake from '@/geometry/SnowFlake.vue'
+import Tree from '@/geometry/Tree.vue'
+
+export default {
+    name: 'scene1',
+    components: {
+        SnowFlake,
+        Tree
+    },
+    initialization:{
+        winter:{
+            size: [70, 70, 70]
+        }
+        floor:{
+            size: [50, 50, 1],
+            color: 'rbga(255,255,255,1)'
+        },
+        SnowFlake:{
+            location: ()
+        }
+        
+    },
+    step:{
+
+    }
+}
+</script>
